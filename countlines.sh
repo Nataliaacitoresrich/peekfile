072fbf42c7bfaf5546fca60477a63be5178951f4
Natalia Acitores Rich <natalia.acitores01@estudiant.upf.edu>
2024-10-30 09:57:25 +0100

    version2

--- countlines.sh
+++ countlines.sh
@@ -1,4 +1,5 @@
+for file in "$@"; do
 countlines=$(cat $1 | wc -l)
 
 if [[ $countlines -eq 0 ]]; then echo "$1 has 0 lines"; elif [[ $countlines -eq 1 ]]; then  echo "$1 has 1 line"; else echo "$1 has >1 line"; fi
-
+done
