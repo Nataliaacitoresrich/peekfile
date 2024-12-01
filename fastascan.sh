6c09d0a250dcecfc567a96ae6d2df4d2f69f7c2b
Natalia Acitores Rich <natalia.acitores01@estudiant.upf.edu>
2024-12-01 19:30:07 +0100

    Final version

--- fastascan.sh
+++ fastascan.sh
@@ -1,6 +1,6 @@
 ####Defining variables for the report
 #creating the variables X, indicating the input folder.
-if [[ -d "$1" ]]; then 		#checking the first argument is a folder, otherwise it could be the input number that has been defined.
+if [[ -d "$1" ]]; then 		#checking the first argument is a folder, otherwise it could be the input number for variable N.
 if [[ ! "$1" ]]
 then X="$(pwd)"
 else
@@ -23,15 +23,18 @@ filename=$(find $X -type f -name "*.fa" -or -name "*.fasta")
 echo The number of Fasta files is $(echo "$filename" | wc -l)
 echo " " #adding an space
 
-#2nd: For each fasta file how many unique Fasta IDs are there: 
-echo "$filename" | while read i      			# finding each fasta file and deriving the paths of the files to a loop 
-do echo ======$i                                        # echoing the name of the fasta file. 
-(grep ">" $i | awk '{split($0,A,/ /); print A[1]}'| sort -n | uniq -c | awk -F' ' '$1==1{print $1}' | sort |uniq -c |awk -F' ' '{print "there are " $1 " unique Fasta IDs" }')
-# grepping the header using ">", then splitting the output by spaces in order to haven only the Fasta ID (print A[1]). afterwards we sorted the Id's and counted them. From this output we now filter to keep only the ones with counts = 1 (unique). After this we filter again to print only the number, not the ID's. 
-done
+#2nd: Printing how many unique Fasta IDs there are in total: 
+echo "$filename" | while read i      			
+do grep ">" $i | awk '
+{split($0,A,/ /); print A[1]}' 			#getting the Fasta IDs, and sorting to get the unique ID's
+done | sort -n | uniq -c | awk -F' ' '		
+$1==1{print $1}					#from the unique output, getting only the numbers, then only keeping the unique Ids
+' | sort |uniq -c |awk -F' ' '
+{print "There are " $1 " unique Fasta IDs" }'		
+
 
 ##3rd: Printing for each file a header and lines to be displayed. 
-aa_non_nt="[RNDQEHILKMFPSWYV]" #defining string of aa that do not match any nucleotide symbol.
+aa_non_nt="[RDQEHILKMFPSWYV]" #defining string of aa that do not match any nucleotide symbol or Ns as is how missmatches are displayed in the sequence. 
 
 #Header:
 for i in $filename
@@ -39,22 +42,30 @@ do echo "======$i"		#printing filename
 if [[ -h "$i" ]]; then
 echo "it is a symlink"		#checking if the file is a symlink
 fi 
-grep ">" "$i" | wc -l		#Number of sequences inside
-grep -v -h ">" "$i" | awk '	#from every fasta file, we want to count the sequence length, in order to do so we first have to remove gaps, spaces and newlines
+echo There are $(grep ">" "$i" | wc -l) sequences in this file	#Number of sequences inside
+
+#from every fasta file, we want to know the total sequences length, in order to do so, we first have to remove gaps, spaces and newlines.
+grep -v -h ">" "$i" | awk '	
 	
         { gsub("-", "") 	#removing gaps "-"
         gsub(" ", "")		#removing spaces
         gsub ("\n","")		#removing newlines
         print $0}
-' |awk  -v aa_nt_awk="$aa_non_nt" ' 				#including the aa_non_nt variable in awk.
-{if ($0~aa_nt_awk) {print "AA" " " length} 			#printing the length of the sequences and if it is an aminoacidic sequence (AA) and nucleotidic sequence (NT)
-else {print "NT" " " length}}' 
-done	
+' | echo The total sequences length is $(wc -m) 	 
 
-####Displaying sequence lines given the condition: if the file has 2N lines or fewer, then display its full content; if not, show the first N lines, then "...", then the last N lines. If N is 0, skip this step.
+# to check if the sequences are nucleotidic or aminoacidic:
+grep -v -h ">" "$i" | awk '	
+	
+        { gsub("-", "") 	#removing gaps "-"
+        gsub(" ", "")		#removing spaces
+        gsub ("\n","")		#removing newlines
+        print $0}
+'|awk  -v aa_nt_awk="$aa_non_nt" ' 							#including the aa_non_nt variable in awk.
+{if ($0~aa_nt_awk) {print "The sequences have aminoacids   AA"} 			#printing if the sequence is composed of aminoacids or nucleotides
+else {print "The sequences have nucleotides   NT"}}' | sort | uniq 	
 
-echo "$filename"| while read i      #extracting the name of each file
-do linecount=$(wc -l "$i"|awk '{split ($0,A,/ /); print A[1]}')		#count the lines and print only the number, not the path to the file
+# Displaying sequence lines given the condition: if the file has 2N lines or fewer, then display its full content; if not, show the first N lines, then "...", then the last N lines. If N is 0, skip this step.
+linecount=$(wc -l "$i"|awk '{split ($0,A,/ /); print A[1]}')		#count the lines and print only the number, not the path to the file
 if [[ "$linecount" -le $((2*N)) ]]; then #setting the condition
 cat "$i"
 else 
